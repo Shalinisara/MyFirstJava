d3d9a010192b0a44c6047f97a9a8526e92658c6f
Shalinisara <shaals009@gmail.com>
2021-02-11 04:39:03 +0000

    added calci

---
+++ ArrayExamp/src/Arraypkg/ArrayMain.java
@@ -0,0 +1,12 @@
+package Arraypkg;
+
+public class ArrayMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a [] = new int [5];
+		System.out.println(a[]);
+
+	}
+
+}
